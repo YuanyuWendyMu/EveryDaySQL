0c9da20434cc60655334ccc142ddb691f2978dc5
Wendy Mu <42749105+YuanyuWendyMu@users.noreply.github.com>
2019-06-16 16:10:21 -0700

    Add files via upload

---
+++ Leetcode/180. Consecutive Numbers.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+SELECT *
+FROM
+    Logs l1,
+    Logs l2,
+    Logs l3
+WHERE
+    l1.Id = l2.Id - 1
+    AND l2.Id = l3.Id - 1
+    AND l1.Num = l2.Num
+    AND l2.Num = l3.Num
+
+#link back to the 603 Consecutive Available Seats
+#Find three consecutive number: join three times
